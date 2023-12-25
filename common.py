47455faecf96cda221cec72b012931edabe714d5
Sjors Smits <smits.sjors@gmail.com>
2023-12-25 16:28:16 +0100

    rename

---
+++ common.py
@@ -0,0 +1,25 @@
+import csv
+from contextlib import contextmanager
+from pathlib import Path
+
+from pyspark import RDD, SparkConf, SparkContext
+
+
+def get_spark_context(app_name: str) -> SparkContext:
+    conf = (
+        SparkConf()
+        .setAppName(app_name)
+        .setMaster("local[*]")
+        .set("spark.eventLog.enabled", "true")
+    )
+
+    sc = SparkContext.getOrCreate(conf)
+    sc.setLogLevel("WARN")
+
+    return sc
+
+
+@contextmanager
+def csv_writer(path: str | Path):
+    with open(path, "w+", newline="") as file:
+        yield csv.writer(file)
